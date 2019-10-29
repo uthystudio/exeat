1fbf75310c7c932996352ec8224d305bf5674b01
GlebKiva <feironox5@gmail.com>
2019-10-29 14:07:38 +0300

    first commit

---
+++ src/app/pages/schedule/schedule.ts
@@ -0,0 +1,57 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, IonList, LoadingController, ModalController, ToastController, Config } from '@ionic/angular';
+
+import { ScheduleFilterPage } from '../schedule-filter/schedule-filter';
+import { ConferenceData } from '../../providers/conference-data';
+import { UserData } from '../../providers/user-data';
+
+@Component({
+  selector: 'page-schedule',
+  templateUrl: 'schedule.html',
+  styleUrls: ['./schedule.scss'],
+})
+export class SchedulePage implements OnInit {
+  // Gets a reference to the list element
+  @ViewChild('scheduleList', { static: true }) scheduleList: IonList;
+
+  ios: boolean;
+  dayIndex = 0;
+  queryText = '';
+  segment = 'all';
+  excludeTracks: any = [];
+  shownSessions: any = [];
+  groups: any = [];
+  confDate: string;
+  customDayShortNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  constructor(
+    public alertCtrl: AlertController,
+    public confData: ConferenceData,
+    public loadingCtrl: LoadingController,
+    public modalCtrl: ModalController,
+    public router: Router,
+    public toastCtrl: ToastController,
+    public user: UserData,
+    public config: Config
+  ) { }
+
+  ngOnInit() {
+    this.updateSchedule();
+
+    this.ios = this.config.get('mode') === 'ios';
+  }
+
+  updateSchedule() {
+  }
+
+  async openSocial(network: string, fab: HTMLIonFabElement) {
+    const loading = await this.loadingCtrl.create({
+      message: `Posting to ${network}`,
+      duration: (Math.random() * 1000) + 500
+    });
+    await loading.present();
+    await loading.onWillDismiss();
+    await fab.close();
+  }
+}
